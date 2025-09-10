f10e3df337fd5b338e51e50ef15f49e1fbedb145
Atharv <atharvtelkar@gmail.com>
2025-09-10 14:12:41 +0530

    fixed bugs

--- obj/Debug/net8.0-windows/Views/DashboardView.g.cs
+++ obj/Debug/net8.0-windows/Views/DashboardView.g.cs
@@ -76,7 +76,7 @@ namespace WpfApp1.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/views/dashboardview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/views/dashboardview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\DashboardView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
